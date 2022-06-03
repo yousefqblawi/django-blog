31ea2ffc613caaa7a5afc0bf4c9ade029a76f0c8
yousefqblawi <yousefnqblawi@gmail.com>
2022-06-03 10:54:59 +0300

    putting templates and static files in the project

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    posts=[
+    {
+    "title":"first post",
+    "author":"yousef",
+    "post_date":"1/5/2020",
+    "content":"this is the first post"
+    },
+    {
+    "title":"second post",
+    "author":"nawar",
+    "post_date":"10/5/2020",
+    "content":"this is the second post"
+    }
+    ]
+    context={"posts":posts}
+    return render(request,"blog/index.html",context)
+def about(request):
+    pass
